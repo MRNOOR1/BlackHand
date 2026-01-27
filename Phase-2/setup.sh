918d19a9cbec564acf9ef548702e415c7939727a
Mohammad Rasoul Noori <mohammadnoori@gmail.com>
2026-01-27 18:51:36 +1100

    b2r updated

--- Phase-2/setup.sh
+++ Phase-2/setup.sh
@@ -15,7 +15,15 @@ docker build -t "${IMAGE_NAME}" "${ROOT_DIR}/docker"
 CUSTOM_DIR="${ROOT_DIR}/custom"
 mkdir -p "${CUSTOM_DIR}"
 
+BLACKHAND_EXTERNAL_DIR="${ROOT_DIR}/blackhand-external"
+
+if [[ ! -d "${BLACKHAND_EXTERNAL_DIR}" ]]; then
+  echo "Missing ${BLACKHAND_EXTERNAL_DIR}. Create the br2-external tree first." >&2
+  exit 1
+fi
+
 docker run --rm -it \
   --name "${CONTAINER_NAME}" \
   -v "${CUSTOM_DIR}:/artifacts" \
+  -v "${BLACKHAND_EXTERNAL_DIR}:/work/blackhand-external" \
   "${IMAGE_NAME}"
